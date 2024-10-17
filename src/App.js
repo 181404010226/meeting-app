1590d71fcaff6e17e7e85d00765bbb4dec260afc
谭龙飞 <12621543+tan-longfei@user.noreply.gitee.com>
2024-10-17 16:48:28 +0800

    路径问题

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import axios from '../services/api';
+import axios from './services/api';
 
 const AppContext = createContext();
 
